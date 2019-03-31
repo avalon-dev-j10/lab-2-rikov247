7f2fe4341c3e0013b58c4da8eaa3ad82e4c82d21
andreyrykov <48448110+rikov247@users.noreply.github.com>
2019-03-31 11:42:55 +0300

    Первый вариант

--- src/ru/avalon/java/dev/j10/labs/shapes/Circle.java
+++ src/ru/avalon/java/dev/j10/labs/shapes/Circle.java
@@ -11,26 +11,42 @@ import java.lang.Math;
  * @see
  * <a href="https://ru.wikipedia.org/wiki/%D0%9E%D0%BA%D1%80%D1%83%D0%B6%D0%BD%D0%BE%D1%81%D1%82%D1%8C">Окружность</a>
  */
-public class Circle implements Ellipse, Shape {
+public class Circle implements Ellipse, Shape, Polygon, Point {
 
-    /*
-     * TODO: Реализовать класс 'Circle'
-     * 1. Используйте наследование.
-     * 2. Реализуйте все абстрактные методы.
-     */
-    private float radius; // переменная радиуса круга
-
-    public Circle(float radius) { // метод описывающий, как ининциализируется круг 
+   private float radius; // радиус круга
+   private float x; // координата x
+   private float y; // координата y
+   
+    public Circle(float x, float y, float radius) { // инициализация круга
+        this.radius = radius;
+        this.x = x;
+        this.y = y;
+    }
+    
+    public Circle(float radius) { // инициализация круга без координат его центра
         this.radius = radius;
     }
 
-    public float getLength() { // метод описывающий, как рассчитывается длина круга = 2*pi*r
+    public float getLength() { // расчёт длины круга = 2*pi*r
         float temp = 2 * ((float) Math.PI) * radius;
         return temp;
     }
 
-    public float getArea() {
+    public float getArea() { // расчёт площади круга = pi*r^2
         float temp = ((float) Math.PI) * radius * radius;
         return temp;
+    }
+    
+    public float getPerimeter() { // расчёт периметра круга = длины окружности
+        float temp = ((float) Math.PI) * radius * radius;
+        return temp;
+    }
+    
+    public float getX() { // возвращение координаты х
+        return x;
+    }
+    
+    public float getY() {  // возвращение координаты y
+        return y;
     }
 }
